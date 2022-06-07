24e9c5a7240952b927f60cc2482dd1377f238493
godo129 <76652929+godo129@users.noreply.github.com>
2022-06-07 18:16:59 +0900

    home

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -3,12 +3,20 @@ import 'package:flutter/material.dart';
 import './dummy_data.dart';
 import './category_item.dart';
 
-class MyWidget extends StatelessWidget {
+class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('DeliMeal'),
+      ),
+      body: GridView(
       children: DUMMY_CATEGORIES
-          .map((catData) => CategoryItem(catData.title, catData.color))
+          .map((catData) => CategoryItem(
+            catData.title, 
+            catData.color
+            )
+          )
           .toList(),
       gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
         maxCrossAxisExtent: 200,
@@ -16,6 +24,7 @@ class MyWidget extends StatelessWidget {
         crossAxisSpacing: 20,
         mainAxisSpacing: 20,
       ),
-    );
+    ),
+    ); 
   }
 }
