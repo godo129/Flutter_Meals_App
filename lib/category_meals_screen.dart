04b9f2b23a31efe5e776719828921b76dc3ed70b
godo129 <76652929+godo129@users.noreply.github.com>
2022-06-08 02:51:24 +0900

    passing data

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 
 class CategoryMealsScren extends StatelessWidget {
+  final String categoryId;
+  final String categoryTitle; 
+
+  CategoryMealsScren(this.categoryId, this.categoryTitle);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('The Recipes'),
+        title: Text(categoryTitle),
       ),
       body: Center(
         child: Text(
