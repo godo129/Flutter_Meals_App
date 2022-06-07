5e51483d63f19ac5aa079a5fa92490c256b0ff9c
godo129 <76652929+godo129@users.noreply.github.com>
2022-06-08 04:49:38 +0900

    ListView 에 더미 데이터 띄우기

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './dummy_data.dart';
+
 class CategoryMealsScren extends StatelessWidget {
 
   static const routeName = '/category-meals';
@@ -13,14 +15,18 @@ class CategoryMealsScren extends StatelessWidget {
     final routeArgs = ModalRoute.of(context).settings.arguments as Map<String, String>;
     final categoryTitle = routeArgs['title'];
     final categoryId = routeArgs['id'];
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle),
       ),
-      body: Center(
-        child: Text(
-        'The Recipes For the Category'
-        ),
+      body: ListView.builder(
+        itemBuilder: (ctx,index) {
+          return Text(categoryMeals[index].title);
+        },
+        itemCount: categoryMeals.length,
       ),
     );
   }
