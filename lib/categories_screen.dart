c5de03d9f7ee482955724856ebb0e29fd7a572ee
godo129 <76652929+godo129@users.noreply.github.com>
2022-06-07 17:55:02 +0900

    base

---
+++ lib/categories_screen.dart
@@ -0,0 +1,21 @@
+
+import 'package:flutter/material.dart';
+
+class MyWidget extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+      children: <Widget>[
+        
+      ],
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
+      ),
+    );
+    
+  }
+}
