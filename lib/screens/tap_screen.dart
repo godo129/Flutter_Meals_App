4cc691b5dbdf322cbf6154f97ad18058b5d92e9a
godo129 <76652929+godo129@users.noreply.github.com>
2022-06-08 21:33:52 +0900

    adding drawer / 사이드 메뉴

--- lib/screens/tap_screen.dart
+++ lib/screens/tap_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../widgets/main_drawer.dart';
 import './favorites_screen.dart';
 import './categories_screen.dart';
 
@@ -28,6 +29,7 @@ class _TabScreenState extends State<TabScreen> {
         appBar: AppBar(
           title: Text(_pages[_selectPageIndex]['title']),
         ),
+        drawer: MainDrawer(),
         body: _pages[_selectPageIndex]['page'],
         bottomNavigationBar: BottomNavigationBar(
           onTap: _selectPage,
